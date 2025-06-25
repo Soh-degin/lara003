61230d9667ef142648e5a4b597a710acf69b2281
LAPTOP-5NOKQRU8\Rise0043 <teshigawara0000sou@gmail.com>
2025-06-25 11:08:47 +0900

    入力フォーム・確認フォームを追加

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ContactsController extends Controller
+{
+    public function index()
+    {
+        // 入力ページを表示
+        return view('contact.index');
+    }
+
+    public function confirm(Request $request)
+    {
+        // バリデーションルールを定義
+        // 引っかかるとエラーを起こしてくれる
+        $request->validate([
+        'email' => 'required|email',
+        'title' => 'required',
+        'body' => 'required',
+        ]);
+
+        // フォームからの入力値を全て取得
+        $inputs = $request->all();
+
+        // 確認ページに表示
+        // 入力値を引数に渡す
+        return view('contact.confirm', [
+        'inputs' => $inputs,
+        ]);
+
+        $request->validate([
+        'email' => 'required|email',
+        'title' => 'required',
+        'body' => 'required',
+        ]);
+
+         // 連想配列形式で取得
+         $inputs = $request->all();
+    }
+}
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>title</title>
+</head>
+<body>
+    <div class="container">
+      @yield('content')
+    </div>
+</body>
+</html>
+
+    
