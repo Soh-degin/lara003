f78726ac2a4ac837b13e64f935d1590a6dd74c33
LAPTOP-5NOKQRU8\Rise0043 <teshigawara0000sou@gmail.com>
2025-06-26 10:42:49 +0900

    入力内容修正が出来ない

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Mail\ContactsSendmail;
 
 class ContactsController extends Controller
 {
@@ -38,7 +39,37 @@ class ContactsController extends Controller
         'body' => 'required',
         ]);
 
-         // 連想配列形式で取得
-         $inputs = $request->all();
+        // 連想配列形式で取得
+        $inputs = $request->all();
+
+        // actionの値を取得
+        $action = $request->input('action');
+
+        // action以外のinputの値を取得
+        $inputs = $request->except('action');
+
+        //actionの値で分岐
+        if($action !== 'submit'){
+
+        // 戻るボタンの場合リダイレクト処理
+        return redirect()
+        ->route('contact.index')
+        ->withInput($inputs);
+        
+        } else {
+        // 送信ボタンの場合、送信処理
+
+        // ユーザにメールを送信
+        \Mail::to($inputs['email'])->send(new ContactsSendmail($inputs));
+        // 自分にメールを送信
+        \Mail::to('自分のメールアドレス')->send(new ContactsSendmail($inputs));
+
+        // 二重送信対策のためトークンを再発行
+        $request->session()->regenerateToken();
+
+        // 送信完了ページのviewを表示
+        return view('contact.thanks');
+        }
     }
 }
+
